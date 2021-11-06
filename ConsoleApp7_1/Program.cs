8e332c97411cec2063be2dd7d2100e010e65a1e0
lazylizzy <petrovalizpetrova@yandex.ru>
2021-11-06 13:49:27 +0300

    Исправлено

--- ConsoleApp7_1/Program.cs
+++ ConsoleApp7_1/Program.cs
@@ -8,51 +8,52 @@ namespace ConsoleApp7_1
 {
     class Program
     {
-        static void Main(string[] args)
+
+        public double x;
+        public double y;
+        public double z;
+        public double p;
+        double Area()
         {
-            Console.Write("Введите x1: ");
-            double x1 = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Введите y1: ");
-            double y1 = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Введите z1: ");
-            double z1 = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Введите x2: ");
-            double x2 = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Введите y2: ");
-            double y2 = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Введите z2: ");
-            double z2 = Convert.ToInt32(Console.ReadLine());
-            double p1 = (x1 + y1 + z1) / 2;
-            double p2 = (x2 + y2 + z2) / 2;            
-            double result = Area1(p1, x1, y1, z1);
-            double result2 = Area2(p2, x2, y2, z2);
-            if (result> result2)
+            return Math.Sqrt(p * (p - x) * (p - y) * (p - z));
+        }
+        class AreaDemo
+        {
+            static void Main()
             {
-                Console.WriteLine("Площадь треугольника со сторонами х1,у1,z1 больше и равна {0}", result);
-                Console.ReadKey();
-            }
-            else if (result2 > result)
-            {
-                Console.WriteLine("Площадь треугольника со сторонами х2,у2,z2 больше и равна{0}", result2);
-                Console.ReadKey();
-            }
-            else
-            {
-                Console.WriteLine("{Площади треугольников равны}") ;
-                Console.ReadKey();
-            }
-            
+                Program first = new Program();
+                Program second = new Program();
+                double areaxyz;
+                first.x = 20;
+                first.y = 20;
+                first.z = 20;
+                first.p = (first.x + first.y + first.z) / 2;
+                second.x = 45;
+                second.y = 45;
+                second.z = 45;
+                second.p = (second.x + second.y + second.z) / 2;
+                areaxyz = first.Area();
+                areaxyz = second.Area();
 
 
-        }
-        static double Area1 (double p1, double x1, double y1, double z1)
-        {
-            return  Math.Sqrt(p1 * (p1 - x1) * (p1 - y1) * (p1 - z1));
-            
-        }
-        static double Area2 (double p2, double x2, double y2, double z2)
-        {
-            return Math.Sqrt(p2 * (p2 - x2) * (p2 - y2) * (p2 - z2));
+
+                if (first.Area() > second.Area())
+                {
+                    Console.WriteLine("Площадь первого треугольника больше и равна {0}", first.Area());
+                    Console.ReadKey();
+                }
+                else if (second.Area() > first.Area())
+                {
+                    Console.WriteLine("Площадь второго треугольника больше  и равна {0}", second.Area());
+                    Console.ReadKey();
+                }
+                else
+                {
+                    Console.WriteLine("{Площади треугольников равны}");
+                    Console.ReadKey();
+                }
+
+            }
         }
     }
 
